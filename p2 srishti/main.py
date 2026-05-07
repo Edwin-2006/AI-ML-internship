b25eeb07d9a230d1c257a296954adbe1e5db04a2
Edwin Biju <u2403107@rajagiri.edu.in>
2026-05-07 13:56:08 +0530

    Add files via upload

---
+++ p2 srishti/main.py
@@ -0,0 +1,38 @@
+from auth import register, login, dashboard
+from exceptions import UserExistsError, UserLoginError
+from util import line
+
+while True:
+    line()
+    print("1.Register")
+    print("2.Login")
+    print("3.Exit")
+
+    choice = input("Enter choice: ")
+
+    try:
+        if choice == "1":
+            register()
+
+        elif choice == "2":
+            user = login()
+            dashboard(user)
+
+        elif choice == "3":
+            print("Exiting...")
+            break
+
+        else:
+            print("Invalid choice")
+
+    except UserExistsError as e:
+        print(e)
+
+    except UserLoginError as e:
+        print(e)
+
+    except Exception as e:
+        print("Error:", e)
+
+    finally:
+        print("Operation completed")
